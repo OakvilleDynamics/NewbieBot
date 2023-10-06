fe34d2f63763673a2799cd3fdee1d5bf36127dad
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2023-10-05 22:15:52 -0500

    Fix issue with RobotContainer
    Fixed an issue where the RobotContainer did not have any subsystems or commands linked to it. This is now fixed and allows for driver control at the specified CAN IDs in the Constants file.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -16,6 +18,10 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
+  private static final RobotContainer m_robotContainer = new RobotContainer();
+
+  public final Drivetrain m_drivetrain = new Drivetrain();
+  public final Pneumatics m_pneumatics = new Pneumatics();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
 
@@ -23,6 +29,10 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+
+    // Set default commands
+    m_drivetrain.setDefaultCommand(new Drive(m_drivetrain));
+    m_pneumatics.setDefaultCommand(new PistonControl(m_pneumatics));
   }
 
   /**
